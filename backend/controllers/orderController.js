5ba0f270260dbf4aaa85501470b1d5f4ef1a7717
mifzalnajid <mifzalnajid@gmail.com>
2025-08-25 08:14:40 +0530

    Stripe to stripe

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
-import Stripe from "Stripe";
+import Stripe from "stripe";
 
 //Global Variables
 const currency = "usd";
